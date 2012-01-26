3219c8906e7a8a141ec49bdee5fb2b287e9445dc
leoncio caminha <leoncio.sobreira@gmail.com>
2012-01-26 20:52:02 -0300

    some things...not done yet

---
+++ javascript/dosasite.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$("#title").click(function () {
+  	$("ul").show("slow");
+  });
+
+	$("#menu_1").click(function () {
+  	$("#menu_11").show("slow");
+  });
+
+
+	$("#menu_2").click(function () {
+  	$("#menu_21").show("slow");
+  });
+
+	$("#menu_3").click(function () {
+  	$("#menu_31").show("slow");
+  });
+
+});
+
